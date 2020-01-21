018f8d98b02f5d30aca148f791943c0817e73d57
Henry <henry_febryan@yahoo.com>
2020-01-21 16:15:12 +0700

    setup & signin FIREBASE (Email)

--- app/src/main/java/com/henry/firemessage/MainActivity.kt
+++ app/src/main/java/com/henry/firemessage/MainActivity.kt
@@ -7,23 +7,24 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_people -> {
+                    true
+                }
+                R.id.navigation_account -> {
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/henry/firemessage/SignInActivity.kt
@@ -0,0 +1,79 @@
+package com.henry.firemessage
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.IdpResponse
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import org.jetbrains.anko.clearTask
+import org.jetbrains.anko.design.longSnackbar
+import org.jetbrains.anko.indeterminateProgressDialog
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.newTask
+
+class SignInActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 1
+
+    private val signInProviders =
+        listOf(
+            AuthUI.IdpConfig.EmailBuilder()
+                .setAllowNewAccounts(true)
+                .setRequireName(true)
+                .build()
+        )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        account_sign_in.setOnClickListener {
+            val intent = AuthUI.getInstance().createSignInIntentBuilder()
+                .setAvailableProviders(signInProviders)
+                .setLogo(R.drawable.ic_account_circle_black_24dp)
+                .build()
+            startActivityForResult(intent, RC_SIGN_IN)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                val progressDialog = indeterminateProgressDialog("Setting up your account")
+                startActivity(intentFor<MainActivity>().newTask().clearTask())
+                progressDialog.dismiss()
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                if (response == null) return
+
+                when (response.error?.errorCode) {
+                    ErrorCodes.NO_NETWORK ->
+                        showSnackbar(constraint_layout, "No Network")
+                    ErrorCodes.UNKNOWN_ERROR ->
+                        showSnackbar(constraint_layout, "Unknown error")
+                }
+            }
+        }
+    }
+
+    fun showSnackbar(view: View, message: String) {
+        val snackbar = Snackbar.make(view, message, Snackbar.LENGTH_LONG)
+            .setAction("Action", null)
+        val sbView = snackbar.view
+        sbView.setBackgroundColor(ContextCompat.getColor(this, R.color.colorAccent))
+        val tv = sbView.findViewById<TextView>(R.id.snackbar_text)
+        tv.setTextColor(Color.WHITE)
+        snackbar.show()
+    }
+}
